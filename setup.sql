63a2fa0f1ae8f200e3be8b96de1c4543612b59a3
Javad <ja7adr@gmail.com>
2021-01-12 09:39:29 +0000

    updated mysql service

--- setup.sql
+++
@@ -1,8 +0,0 @@
--- create the databases
-CREATE DATABASE IF NOT EXISTS wordpress;
-
--- create the users for each database
-CREATE USER 'mywp'@'%' IDENTIFIED BY 'xx11223XX@';
-GRANT CREATE, ALTER, INDEX, LOCK TABLES, REFERENCES, UPDATE, DELETE, DROP, SELECT, INSERT ON `wordpress`.* TO 'mywp'@'%';
-
-FLUSH PRIVILEGES;
